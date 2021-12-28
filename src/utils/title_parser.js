2ced94eaa4d45d449c5e9ac19456f41f167ba52c
cakoshakib <cakoshakib@gmail.com>
2021-12-28 17:45:58 -0500

    Better parsing

--- src/utils/title_parser.js
+++ src/utils/title_parser.js
@@ -1,92 +1,101 @@
 const removeBracketed = (title) => {
-  return title.replace(/[\s_.]*\[[^\]]*\][\s_.]*/g, "")
-}
+  return title.replace(/[\s_.]*\[[^\]]*\][\s_.]*/g, "");
+};
 
 const removeParends = (title) => {
-  return title.replace(/ *\([^)]*\) */g, "")
-}
+  return title.replace(/ *\([^)]*\) */g, "");
+};
 
 const validEp = (episode) => {
-  return episode && !isNaN(episode[1])
-}
+  return episode && !isNaN(episode[1]);
+};
 
 const getEpisode = (details) => {
-  let parts = [details]
-  
-  if (details.includes('-')) {
-    parts = parts.concat(details.substring(0, details.indexOf('-')).trim())
-    parts = parts.concat(details.substring(details.indexOf('-') + 1).trim())
+  let parts = [details];
+
+  if (details.includes("-")) {
+    parts = parts.concat(details.substring(0, details.indexOf("-")).trim());
+    parts = parts.concat(details.substring(details.indexOf("-") + 1).trim());
   }
 
   for (let part of parts) {
     // ep1, episode 1, e1
-    let match = part.match(/(episode\s?|ep|e)(\d+)/i)
-    if (match && !isNaN(match[2])) return [part.replace(match[0], ''), match[2]]
+    let match = part.match(/(episode\s?|ep|e)(\d+)/i);
+    if (match && !isNaN(match[2]))
+      return [part.replace(match[0], ""), match[2]];
     // 5x3 (season 5 ep 3)
-    match = part.match(/\d+x(\d+)/i)
-    if (validEp(match)) return [part.replace(match[0], ''), match[1]]
+    match = part.match(/\d+x(\d+)/i);
+    if (validEp(match)) return [part.replace(match[0], ""), match[1]];
     // 1v3 (episode 1 season 3)
-    match = part.match(/(\d+)v\d+/i)
-    if (validEp(match)) return [part.replace(match[0], ''), match[1]]
-    // 1 (just the number)
-    match = part.match(/(\d+)$/i)
-    if (validEp(match)) return [part.replace(match[0], ''), match[1]]
+    match = part.match(/(\d+)v\d+/i);
+    if (validEp(match)) return [part.replace(match[0], ""), match[1]];
+    // 1 (just the number, at the end)
+    match = part.match(/(\d+)$/i);
+    if (validEp(match)) return [part.replace(match[0], ""), match[1]];
+    // 1 (just the number, anywhere)
+    match = part.match(/(\d+)/i);
+    if (validEp(match)) return [part.replace(match[0], ""), match[1]];
   }
 
-  return [part, '']
-}
+  return [parts, ""];
+};
 
 const splitDash = (title) => {
-  const parsedTitle = title.substring(0, title.indexOf('-')).trim()
-  const details = title.substring(title.indexOf('-') + 1).trim()
+  const parsedTitle = title.substring(0, title.indexOf("-")).trim();
+  const details = title.substring(title.indexOf("-") + 1).trim();
 
-  const episode = getEpisode(details)[1]
-  
+  const episode = getEpisode(title)[1];
+
   return {
-    'title': parsedTitle,
-    'details': details,
-    'episode': episode
-  }
-}
+    title: parsedTitle,
+    details: details,
+    episode: episode,
+  };
+};
 
 const removeFileExtension = (title) => {
-  return title.substring(0, title.lastIndexOf('.'))
-}
+  return title.substring(0, title.lastIndexOf("."));
+};
 
 const replaceDelimiter = (title) => {
-  return title.replace(/\./g, ' ').replace(/\_/g, ' ')
-}
+  return title.replace(/\./g, " ").replace(/\_/g, " ");
+};
 
 const parseTitle = (title) => {
-  let parsedTitle = title
-  title = title.trim()
+  let parsedTitle = title;
+  title = title.trim();
 
-  if (title.includes('.mkv') || title.includes('.mp4')) {
-    parsedTitle = removeFileExtension(parsedTitle)
+  // Special ' character
+  if (title.includes("&#39;")) {
+    parsedTitle = parsedTitle.replace("&#39;", "'");
   }
 
-  if (title.includes(']')) {
-    parsedTitle = removeBracketed(parsedTitle)
+  if (title.includes(".mkv") || title.includes(".mp4")) {
+    parsedTitle = removeFileExtension(parsedTitle);
   }
 
-  if (title.includes(')')) {
-    parsedTitle = removeParends(parsedTitle)
+  if (title.includes("]")) {
+    parsedTitle = removeBracketed(parsedTitle);
   }
 
-  if (!parsedTitle.includes(' ')) {
-    parsedTitle = replaceDelimiter(parsedTitle)
+  if (title.includes(")")) {
+    parsedTitle = removeParends(parsedTitle);
   }
 
-  if (title.includes('-')) {
-    return splitDash(parsedTitle)
+  if (!parsedTitle.includes(" ")) {
+    parsedTitle = replaceDelimiter(parsedTitle);
   }
 
-  const info = getEpisode(parsedTitle)
+  if (title.includes("-")) {
+    return splitDash(parsedTitle);
+  }
+
+  const info = getEpisode(parsedTitle);
 
   return {
-    'title': info[0].trim(),
-    'episode': info[1].trim()
-  }
-}
+    title: info[0].trim(),
+    episode: info[1].trim(),
+  };
+};
 
-module.exports = parseTitle
+module.exports = parseTitle;
